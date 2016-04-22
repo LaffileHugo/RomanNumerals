bb60602db53be5155ef0f8d5db02c33cef234ca0
Laffilé Hugo <hugo.lfl@orange.fr>
2016-04-22 09:40:57 +0200

    Premier comit

--- src/main/java/iut/tdd/RomanNumerals.java
+++ src/main/java/iut/tdd/RomanNumerals.java
@@ -1,11 +1,23 @@
 package iut.tdd;
 
 public class RomanNumerals {
+	
+	String[] tabRoman = new String[]{"I","V","X"}; 
+	
 	public String convertToRoman(String arabe) {
-		return "I";
+		
+		int stringToInt = Integer.parseInt(arabe);
+		String res="";
+		
+		if(stringToInt < 4 ){
+			for(int idx=0;idx<stringToInt;idx++){
+				res+=tabRoman[0];
+			}
+		}
+		return res;
 	}
 
-	public Object convertFromRoman(String roman) {
+	public String convertFromRoman(String roman) {
 		return "1";
 	}
 
